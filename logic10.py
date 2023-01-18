fae835530613272dae28b9e675d244c4f9944220
bolbekus <bolbekus@gmail.com>
2023-01-18 10:29:10 +0500

    10

--- logic10.py
+++ logic10.py
@@ -1,9 +1,13 @@
 def main(a):
     """
-    Given integer a,  check the following statement "The integer is two-digit number".
+    Given integer a,  \
+    check the following statement "The integer is two-digit number".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
+    return a > 9 
+
+
+print(main(12))
