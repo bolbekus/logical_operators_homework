52b7844593f882d6cd628d146ce73f3767629bcc
bolbekus <bolbekus@gmail.com>
2023-01-18 11:08:04 +0500

    13

--- logic13.py
+++ logic13.py
@@ -1,9 +1,15 @@
 def main(a):
     """
-    Given a two-digit integer a,  check the following statement "All digits sum is even".
+    Given a two-digit integer a, \
+     check the following statement "All digits sum is even".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
+    x1 = a % 10 
+    x2 = a // 10
+
+    return x1 % 2 == 0 + x2 % 2 == 0
+
+print(main(8))
